837a5887398942638c6ee56639bf3ccb0455e7eb
ShreetiShrestha1 <shreetishrestha94@gmail.com>
2025-06-15 14:33:47 +0545

    django project LMS: Assignment model added and migrated

--- core/models.py
+++ core/models.py
@@ -36,8 +36,8 @@ class Teacher(models.Model):
         ('BCE','Bachelor of Civil Engineering')
     ]
     full_name = models.CharField(max_length=200, null=False , blank=False, verbose_name="Teacher Full Name")
-    email = models.CharField(max_length=100, unique=True, null=False, blank=False, verbose_name="Student Email")
-    semester = models.CharField(max_length = 20,choices=DEPARTMENT, default='N/A', null =True, blank=True)
+    email = models.CharField(max_length=100, unique=True, null=False, blank=False, verbose_name="Teacher Email")
+    department = models.CharField(max_length = 20,choices=DEPARTMENT, default='N/A', null =True, blank=True)
     phone_no = models.IntegerField(null=False, blank=False)
     join_date = models.DateField(default='Join Date')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -49,3 +49,22 @@ class Teacher(models.Model):
 
     def __str__ (self):
         return self.full_name
+    
+class Assignment(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Assignment Title')
+    start_date = models.DateField(default='Start Date', null=False, blank=False, verbose_name='Start Date')    
+    end_date = models.DateField(default='End Date', null=False, blank=False, verbose_name='End Date')
+    question_file = models.FileField(upload_to='assigment/questions/', null=True, blank=True, verbose_name='Select Assignment File')
+    question = models.TextField(null=True, blank=True, verbose_name='Assignment Question')
+    remark = models.CharField(max_length=100, null=False, blank=False,verbose_name='Assignment Details')
+    full_mark = models.FloatField(blank= False, null=False)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name='Uploaded By')
+
+
+    class Meta:
+        verbose_name ='assignment'
+        verbose_name_plural ='assignments'
+        ordering =['-title']
+
+    def __str__(self):
+        return self.title     
