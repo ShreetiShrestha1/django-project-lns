4f90f9430f3f694b620d8de94bea78f974defacf
ShreetiShrestha1 <shreetishrestha94@gmail.com>
2025-06-18 14:51:10 +0545

    Django project LSM :Created teacherdetails, delete, update,edit and added css and bootstrap links

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from core.views import HomePageView, TeacherCreateView, TeacherListView
+from core.views import( HomePageView, TeacherCreateView, TeacherListView, TeacherDeleteView,TeacherDetailView,TeacherUpdateView,
+StudentCreateView, StudentListView)
 
 urlpatterns = [ 
     #path('', views.home, name='home'),
     path('', HomePageView.as_view(), name='home'),
-    path('teacher/list/', TeacherListView.as_view(), name='teacher.index'),
-    path('teacher/create/', TeacherCreateView.as_view(), name='teacher.create')
 
+    
+    path('teacher/list/', TeacherListView.as_view(), name='teacher.index'),
+    path('teacher/create/', TeacherCreateView.as_view(), name='teacher.create'),
+    path('teacher/<int:pk>/detail/', TeacherDetailView.as_view(), name='teacher.detail'),
+    path('teacher/<int:pk>/edit/', TeacherUpdateView.as_view(), name='teacher.edit'),
+    path('teacher/<int:pk>/detete/', TeacherDeleteView.as_view(), name='teacher.delete'),
+
+
+    path('student/list/', StudentListView.as_view(), name='student.index'),
+    path('student/create/', StudentCreateView.as_view(), name='student.create')
 ]
